657f045489c511d0fd1b2737de845195febafafb
Soumya Ranjan Swain <110282294+Soumya6610@users.noreply.github.com>
2024-08-27 21:26:19 +0530

    Update register.component.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,7 +10,7 @@ import { User } from '../../models/user.model';
 })
 export class RegisterComponent {
   user: User = {
-    name: '',
+    username: '',
     email: '',
     password: '',
     role: 'OD',
